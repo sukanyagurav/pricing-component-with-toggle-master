f9db30569938dd1528556d12774d494d816151d1
Sukanya Gurav <sukanyagurav6@gmail.com>
2023-05-27 14:11:30 +0530

    updated styles

--- script.js
+++ script.js
@@ -7,20 +7,15 @@ checkbox.addEventListener("change",(e)=>{
     e.preventDefault()
     if(!checkbox.checked){
         annual.style.color='hsl(237, 63%, 64%)';
-      
         prices.forEach((price)=>{ 
-
             price.innerText =((+price.innerText) + (180)) + (gap * i) ;
-        
             gap+=45;
             i++;
         })
     }
     else{  
-       
         annual.style.color='hsl(233, 13%, 49%)';
         prices.forEach((price)=>{   
-        
             price.innerText =(((+price.innerText) - 180) - (gap * i)).toFixed(2) ;
             gap+=45;
             i++;
